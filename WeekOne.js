ee33399aa3dfffaff522c089f34c8ce7a8952f79
mpowell14 <mpowell14@users.noreply.github.com>
2016-10-01 10:51:41 -0500

    Added Activity

---
+++ WeekOne.js
@@ -0,0 +1,16 @@
+function startSecondPuzzle() {
+  $("#puzzleTwo").show();
+}
+
+function startThirdPuzzle(key) {
+  if(key === "GETHIP") {
+    $("#puzzleThree").show();
+  }
+}
+
+function finishPuzzles() {
+  x = 4;
+  y == 5;
+  z = x + y
+  $("#congratulations").show();
+}
